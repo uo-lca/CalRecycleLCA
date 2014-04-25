869a3e90aea940e711a8c57292f2130275cacc8a
Sabina <Sabina.Beraha@ucsb.edu>
2014-04-25 10:18:37 -0700

    Remove users that are not Web.config

--- DatabaseProject/LCAToolDev/LCAToolDev/Security/sabina.sql
+++
@@ -1,2 +0,0 @@
-﻿CREATE USER [sabina] FOR LOGIN [sabina];
-
--- DatabaseProject/LCAToolDev/LCAToolDev/Security/sabina_1.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [sabina]
-    WITH PASSWORD = N'k|sx3pczfp{PnprYzeKCylrFmsFT7_&#$!~<~ucopkt^gumo', SID = 0x1674A070C4809F4DB1855D428C42A409, DEFAULT_DATABASE = [LCAToolDev], DEFAULT_LANGUAGE = [us_english], CHECK_POLICY = OFF;
-
