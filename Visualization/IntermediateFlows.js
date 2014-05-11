6e6e135e65b51fdb7871ac0977d4ca72fad8009a
Sabina Beraha <sabina.beraha@ucsb.edu>
2014-05-11 13:25:49 -0700

    Vis: Transform IntermediateFlows to graph and display as sankey diagram.

--- Visualization/IntermediateFlows.js
+++ Visualization/IntermediateFlows.js
@@ -18,7 +18,8 @@ function processFlow() {
         processesURL = baseURI + "process",
         intFlowURL = "IntermediateFlows.json",
         // Current selections
-        selectedProcessID = 3;
+        selectedProcessID = 3,
+        processName = "CA Waste Code 222_2010";
     /**
      * d3 variables
      */
@@ -29,8 +30,7 @@ function processFlow() {
         left: 20
     },
         width = 600 - margin.left - margin.right,
-        svgHeight = 1000,
-        chartHeight = 100;
+        height = 500 - margin.top - margin.bottom;
 
     var color = d3.scale.ordinal();
 
@@ -40,20 +40,154 @@ function processFlow() {
     var labelFormat = d3.format("^.2g"); // Format numbers with precision 2;
 
     var svg;
+    /**
+      * sankey variables
+      */
+    var sankey = d3.sankey()
+        .nodeWidth(15)
+        .nodePadding(10)
+        .size([width, height]),
+        graph = {};
 
     /**
      * Initial preparation of svg element.
      */
     function prepareSvg() {
-        svg = d3.select("#chartcontainer").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", svgHeight);
-        svg.append("g")
-            .attr("class", "chartgroup")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        svg.append("g")
-            .attr("class", "legendgroup")
-            .attr("transform", "translate(" + margin.left + "," + (chartHeight + margin.top) + ")");
+        svg = d3.select("#chartcontainer")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    }
+
+    /**
+      * Create or refresh sankey diagram with graph data
+      */
+
+    function updateSankey() {
+
+        var link, node, bars, sliders,
+            path = sankey.link();
+
+        sankey.nodes(graph.nodes)
+            .links(graph.links)
+            .layout(32);
+
+        link = svg.append("g").selectAll(".link")
+            .data(graph.links)
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", path)
+            .style("stroke-width", function (d) {
+                return Math.max(1, d.dy);
+            })
+            .sort(function (a, b) {
+                return b.dy - a.dy;
+            });
+
+
+
+        link.append("title")
+            .text(function (d) {
+                return d.source.name + " → " + d.target.name + "\n" + d3.format(d.value);
+            });
+
+        node = svg.append("g").selectAll(".node")
+            .data(graph.nodes)
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            })
+            .call(d3.behavior.drag()
+                .origin(function (d) {
+                    return d;
+                })
+                .on("dragstart", function () {
+                    this.parentNode.appendChild(this);
+                })
+                .on("drag", function dragmove(d) {
+                    d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+                    sankey.relayout();
+                    link.attr("d", path);
+                }));
+
+
+        bars = node.append("rect")
+            .attr("height", function (d) {
+                return d.dy;
+            })
+            .attr({
+                width: sankey.nodeWidth()
+            })
+            .style("fill", function (d) {
+                //                d.color = color(d.name.replace(/ .*/, ""));
+                d.color = colorbrewer.BuGn[3][1];
+                return d.color;
+            })
+            .style("stroke", function (d) {
+                return d3.rgb(d.color).darker(2);
+            });
+        bars.append("title")
+            .text(function (d) {
+                return d.name + "\n" + d3.format(d.value);
+            });
+
+        node.append("text")
+            .attr("x", -6)
+            .attr("y", function (d) {
+                return d.dy / 2;
+            })
+            .attr("dy", ".35em")
+            .attr("text-anchor", "end")
+            .attr("transform", null)
+            .text(function (d) {
+                return d.name;
+            })
+            .filter(function (d) {
+                return d.x < width / 2;
+            })
+            .attr("x", 6 + sankey.nodeWidth())
+            .attr("text-anchor", "start");
+
+    }
+
+    /**
+     * Callback function for IntermediateFlows.
+     * @param {String} error    Web API GET error
+     * @param {Array} data      JSON data from web API
+     */
+    function buildGraph(error, data) {
+        var process;	// Current process
+
+        if (error) {
+            window.alert("Error loading intermediate flows: " + error);
+            return false;
+        }
+
+		graph.nodes = [];
+        graph.links = [];
+
+        graph.nodes.push({name: processName});
+        data.forEach( function (element, index) {
+            var node, link;
+
+            node = {name: element.FlowName};
+            graph.nodes.push(node);
+//            link = {value: +element.ProcessFlowResult};
+            link = {value: 1};
+            if (element.FlowDirection == "Input") {
+                link.source = index+1;
+                link.target = 0;
+            }
+            else {
+                link.source = 0;
+                link.target = index+1;
+            }
+            graph.links.push(link);
+        });
+        updateSankey();
     }
 
 
@@ -61,8 +195,7 @@ function processFlow() {
      * Display intermediate product flows for selected process.
      */
     function displayResults() {
-
-        d3.json(intFlowURL, visualizeResults);
+        d3.json(intFlowURL, buildGraph);
     }
 
     /**
@@ -73,17 +206,10 @@ function processFlow() {
         var paramURL;
 
         selectedProcessID = this.options[this.selectedIndex].value;
+        processName = this.options[this.selectedIndex].text;
         displayResults();
     }
 
-    /**
-     * Callback function for IntermediateFlows
-     * @param {string} error          Web API GET error
-     * @param {Array} results  JSON data from web API
-     */
-    function visualizeResults(error, results) {
-
-    }
 
     /**
      * Starting point for IntermediateFlows
