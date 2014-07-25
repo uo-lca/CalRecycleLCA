cff480b0a12d4fcae3a686edef3a30609e3b2d1b
uo-lca <Sabina.Beraha@ucsb.edu>
2014-07-25 15:41:30 -0700

    WebAPI: Add method that lists flow properties related to fragment

---
+++ vs/LCIAToolAPI/Services/IFlowPropertyService.cs
@@ -0,0 +1,15 @@
+﻿using Data;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IFlowPropertyService : IService<FlowProperty>
+    {
+        IEnumerable<FlowPropertyModel> GetFlowPropertiesByFragment(int fragmentId);            
+    }
+}
