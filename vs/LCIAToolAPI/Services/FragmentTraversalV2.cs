6cc81e4c31a2deb32e9468996fbad999ebf48be3
Rach81 <rachel.scanlon@gmail.com>
2014-07-28 11:22:58 -0700

    pushed my changes because they were causing a conflict when I tried to pull sabina's schema changes.

---
+++ vs/LCIAToolAPI/Services/FragmentTraversalV2.cs
@@ -0,0 +1,15 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    //Version 2 of fragment traversal - rewritten to reflect the pseudocode dated Mon Jul 28 00:32:01 -0700 2014
+    public class FragmentTraversalV2<TEntity> : IService<TEntity> where TEntity : class
+    {
+
+    }
+}
---
+++ vs/LCIAToolAPI/Services/IFragmentTraversalV2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    //Version 2 of fragment traversal - rewritten to reflect the pseudocode dated Mon Jul 28 00:32:01 -0700 2014
+    public interface IFragmentTraversalV2
+    {
+    }
+}
