eb5da14a97132e860e1de799984d6ed2ca5a3ee3
uo-lca <Sabina.Beraha@ucsb.edu>
2014-07-30 10:16:36 -0700

    Vis: Remove test data, no longer needed

--- Visualization/TestData/flows.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT FlowID,
-    concat('"', Name, '"') as Name,
-    --Name
-    ISNULL(CASNumber, '') as CASNumber,
-    FlowTypeID,
-    ReferenceFlowProperty AS ReferenceFlowPropertyID
-FROM Flow f 
