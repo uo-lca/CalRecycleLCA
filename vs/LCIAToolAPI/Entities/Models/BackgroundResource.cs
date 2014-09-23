900f26384c1a58466017fa1df30d258b5fb40e38
Brandon Kuczenski <bkuczenski@bren.ucsb.edu>
2014-09-23 04:04:23 -0700

    Scenario and FragmentLCIA Resources

---
+++ vs/LCIAToolAPI/Entities/Models/BackgroundResource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models {
+    /// <summary>
+    /// Web API resource for background information.
+    /// </summary>
+    public class BackgroundResource {
+        
+        public int FlowID { get; set; }
+        public int DirectionID { get; set; }
+        public int NodeTypeID { get; set; }
+        // if NodeTypeID = 1
+        public int? TargetProcessID { get; set; }
+        // if NodeTypeID = 2
+        public int? TargetFragmentID { get; set; }
+    }
+}
