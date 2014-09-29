7123d178782ccfd2c6fe323b8d508fbb90ab1473
Rach81 <rachel.scanlon@gmail.com>
2014-09-29 11:52:46 -0700

    commit entity to branch

---
+++ vs/LCIAToolAPI/Repository/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class Entity : IObjectState
+    {
+        [NotMapped]
+        public ObjectState ObjectState { get; set; }
+    }
+}
