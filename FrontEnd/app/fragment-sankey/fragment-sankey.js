46736d95c2a704589d2f9b9f65f656063351aa60
Sabina Beraha <Sabina.Beraha@ucsb.edu>
2014-11-06 10:12:02 -0800

    Vis: Use angular ui bootstrap directive to display error message (#46)

--- FrontEnd/app/fragment-sankey/fragment-sankey.js
+++ FrontEnd/app/fragment-sankey/fragment-sankey.js
@@ -1,7 +1,8 @@
 'use strict';
 /* Controller for Fragment Sankey Diagram View */
 angular.module('lcaApp.fragment.sankey',
-                ['ui.router', 'lcaApp.sankey', 'lcaApp.resources.service', 'lcaApp.idmap.service', 'angularSpinner'])
+                ['ui.router', 'lcaApp.sankey', 'lcaApp.resources.service', 'lcaApp.idmap.service', 'angularSpinner',
+                 'ui.bootstrap.alert'])
     .controller('FragmentSankeyCtrl',
         ['$scope', '$stateParams', 'usSpinnerService', '$q', '$window',
         'ScenarioService', 'FragmentService', 'FragmentFlowService', 'FlowForFragmentService', 'ProcessService',
@@ -139,19 +140,25 @@ angular.module('lcaApp.fragment.sankey',
                 }
             }
 
+            function startWaiting() {
+                $scope.alert = null;
+                usSpinnerService.spin("spinner-lca");
+            }
+
             function stopWaiting() {
                 usSpinnerService.stop("spinner-lca");
             }
 
             function handleFailure(errMsg) {
                 stopWaiting();
-                $window.alert(errMsg);
+                $scope.alert = { type: "danger", msg: errMsg };
             }
 
             /**
              * Prepare fragment data for visualization
              */
             function visualizeFragment() {
+                $scope.alert = null;
                 $scope.fragment = FragmentService.get(fragmentID);
                 if ($scope.fragment) {
                     setFlowProperties();
@@ -167,6 +174,7 @@ angular.module('lcaApp.fragment.sankey',
              * Function called after requests for resources have been fulfilled.
              */
             function handleSuccess() {
+
                 $scope.scenario = ScenarioService.get(scenarioID);
                 if ($scope.scenario) {
                     visualizeFragment();
@@ -225,6 +233,7 @@ angular.module('lcaApp.fragment.sankey',
              * If successful, visualize selected fragment.
              */
             function getDataForFragment() {
+                startWaiting();
                 $q.all([FlowPropertyForFragmentService.load({fragmentID: fragmentID}),
                     FragmentFlowService.load({scenarioID: scenarioID, fragmentID: fragmentID}),
                     FlowForFragmentService.load({fragmentID: fragmentID})])
@@ -316,7 +325,7 @@ angular.module('lcaApp.fragment.sankey',
                 }
             }
 
-            usSpinnerService.spin("spinner-lca");
+            startWaiting();
             $scope.color = { domain: ([2, 3, 4, 1, 0]), range: colorbrewer.Set3[5], property: "nodeTypeID" };
             $scope.selectedFlowProperty = null;
             $scope.selectedNode = null;
