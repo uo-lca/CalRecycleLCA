5033ee5af26f04fcd61096b78aca5fb4d284d27a
Sabina Beraha <Sabina.Beraha@ucsb.edu>
2015-02-12 16:13:39 -0800

    Vis: Display visual cues for CF param value change status (#144)

--- FrontEnd/app/lcia-method/lcia-method-detail.js
+++ FrontEnd/app/lcia-method/lcia-method-detail.js
@@ -3,13 +3,11 @@
 angular.module('lcaApp.lciaMethod.detail',
     ['ui.router', 'lcaApp.resources.service', 'ui.bootstrap.accordion', 'ngGrid', 'lcaApp.status.service',
      'lcaApp.models.param', 'lcaApp.models.scenario'])
-    // Follow new angular naming convention for controllers
-    // TODO: refactor other controllers (*Ctrl -> *Controller)
     .controller('LciaMethodDetailController', [
-        '$scope', '$stateParams', '$q', '$log',
+        '$scope', '$stateParams', '$q', '$log', '$window',
         'ImpactCategoryService', 'LciaMethodService', 'FlowForFlowTypeService', 'LciaFactorService',
         'ScenarioModelService', 'ParamModelService', 'PARAM_VALUE_STATUS', 'StatusService',
-        function ($scope, $stateParams, $q, $log,
+        function ($scope, $stateParams, $q, $log, $window,
                   ImpactCategoryService, LciaMethodService, FlowForFlowTypeService, LciaFactorService,
                   ScenarioModelService, ParamModelService, PARAM_VALUE_STATUS, StatusService) {
 
@@ -42,6 +40,28 @@ angular.module('lcaApp.lciaMethod.detail',
                 return row.entity.editStatus === PARAM_VALUE_STATUS.invalid;
             };
 
+            /**
+             * Function to check if Apply Changes button should be disabled.
+             * @returns {boolean}
+             */
+            $scope.noValidChanges = function () {
+                return !($scope.paramScenario && $scope.lciaFactors.length > 0 &&
+                    ScenarioModelService.canUpdate($scope.paramScenario) &&
+                    $scope.lciaFactors.some( function(lf) {
+                        return lf.editStatus === PARAM_VALUE_STATUS.changed;
+                    }) &&
+                    !$scope.lciaFactors.some( function(lf) {
+                        return lf.editStatus === PARAM_VALUE_STATUS.invalid;
+                    }));
+            };
+
+            $scope.applyChanges = function () {
+                var changedParams = $scope.lciaFactors.filter( function (lf) {
+                    return lf.editStatus === PARAM_VALUE_STATUS.changed;
+                });
+                return changedParams.map( getParamChange);
+            };
+
             StatusService.startWaiting();
             $q.all([LciaMethodService.load(), ImpactCategoryService.load(), ScenarioModelService.load(),
                 FlowForFlowTypeService.load({flowTypeID: 2}) ,
@@ -49,34 +69,44 @@ angular.module('lcaApp.lciaMethod.detail',
                 handleLciaFactorResults, StatusService.handleFailure);
 
             function defineParamCol() {
-                var paramCol = {field: 'value', displayName: 'Parameter'};
+                var paramCol = [{field: 'value', displayName: 'Parameter', enableCellEdit: false },
+                                {field: 'editStatus', displayName: '', enableCellEdit: false, width: 20}] ;
                 if ($scope.paramScenario) {
-                    paramCol.visible = true;
+                    paramCol[0].visible = true;
+
                     if (ScenarioModelService.canUpdate($scope.paramScenario)) {
-                        paramCol.cellTemplate = 'template/grid/edit-param-cell.html';
-                        paramCol.enableCellEdit = true;
+                        // Unable to load cell template from file without browser error. Appears to be an ng-grid glitch.
+                        paramCol[0].enableCellEdit = true;
+                        paramCol[1].cellTemplate =
+'<span class="glyphicon glyphicon-ok" aria-hidden="true" ng-show="validChange(row)"></span><span class="glyphicon glyphicon-remove" aria-hidden="true" ng-show="invalidChange(row)"></span>';
+                        paramCol[1].visible = true;
+
+                    } else {
+                        paramCol[1].visible = false;
                     }
                 } else {
-                    paramCol.visible = false;
+                    paramCol[0].visible = false;
+                    paramCol[1].visible = false;
                 }
                 return paramCol;
             }
 
 
-            function setGridColumns () {
+            function setGridColumns() {
                 var paramCol = defineParamCol();
                 $scope.gridColumns = [
                     {field: 'category', displayName: 'Flow Category', enableCellEdit: false},
                     {field: 'name', displayName: 'Flow Name', enableCellEdit: false},
                     {field: 'factor', displayName: 'Factor', cellFilter: 'numFormat', enableCellEdit: false},
-                    paramCol
+                    paramCol[0],
+                    paramCol[1]
                 ];
             }
 
             function displayLciaFactors() {
                 var lciaFactors = LciaFactorService.getAll(),
                     gridHeight = lciaFactors.length * 30 + 50;
-                $scope.dynamicGridStyle = { height: gridHeight} ;
+                $scope.dynamicGridStyle = { height: gridHeight};
                 $scope.lciaFactors =
                     lciaFactors.map(function (f) {
                         var flow = FlowForFlowTypeService.get(f.flowID);
@@ -120,16 +150,16 @@ angular.module('lcaApp.lciaMethod.detail',
             function addParamData() {
 
                 StatusService.handleSuccess();
-                $scope.lciaFactors.forEach( function(lf) {
+                $scope.lciaFactors.forEach(function (lf) {
                     var param = ParamModelService
                         .getLciaMethodFlowParam($scope.paramScenario.scenarioID, $scope.lciaMethod.lciaMethodID,
-                                                lf.flowID);
+                        lf.flowID);
                     if (param) {
                         lf.paramID = param.paramID;
                         lf.value = param.value;
                     } else {
                         lf.paramID = null;
-                        lf.value = null;
+                        lf.value = "";
                     }
                     lf.editStatus = PARAM_VALUE_STATUS.unchanged;
                 });
@@ -137,9 +167,9 @@ angular.module('lcaApp.lciaMethod.detail',
             }
 
             function clearParamData() {
-                $scope.lciaFactors.forEach( function(lf) {
+                $scope.lciaFactors.forEach(function (lf) {
                     lf.paramID = null;
-                    lf.value = null;
+                    lf.value = "";
                     lf.editStatus = PARAM_VALUE_STATUS.unchanged;
                 });
                 setGridColumns();
@@ -160,51 +190,49 @@ angular.module('lcaApp.lciaMethod.detail',
                 getParams();
             }
 
-            function handleCellEdit (evt){
-                // Detect changes and set status
-                //{flowID: 20, category: "Emissions to air, unspecified", name: "pentachlorophenol", factor: 0.00000872, paramID: null…}
+            /**
+             * Handle changes to editable cell
+             * @param evt   Event containing row changed.
+             */
+            function handleCellEdit(evt) {
                 var rowObj = evt.targetScope.row.entity;
-                // parameter value must be a number that differs from factor value
-                if ( rowObj.value && (isNaN(rowObj.value) || +rowObj.value === rowObj.factor)) {
-                    //StatusService.handleFailure("Parameter value, " + value + ", is not numeric.");
-                    rowObj.editStatus = PARAM_VALUE_STATUS.invalid;
-                }
-                else {
-                    if ( rowObj.paramID ) {
+                if (rowObj.value) {
+                    // Param value entered
+                    if (isNaN(rowObj.value) )
+                    {
+                        $window.alert("Parameter value, " + rowObj.value + ", is not numeric.");
+                        rowObj.editStatus = PARAM_VALUE_STATUS.invalid;
+                    } else if (+rowObj.value === rowObj.factor) {
+                        $window.alert("Parameter value, " + rowObj.value + ", is the same as factor value.");
+                        rowObj.editStatus = PARAM_VALUE_STATUS.invalid;
+                    } else if (rowObj.paramID) {
+                        // Check if param value changed
                         var origParam = ParamModelService
                             .getLciaMethodFlowParam($scope.paramScenario.scenarioID, $scope.lciaMethod.lciaMethodID,
-                                                    rowObj.flowID);
+                            rowObj.flowID);
                         if (origParam.value === +rowObj.value) {
                             rowObj.editStatus = PARAM_VALUE_STATUS.unchanged;
                         } else {
                             rowObj.editStatus = PARAM_VALUE_STATUS.changed;
                         }
-                    }
-                    else if (rowObj.value) {
+                    } else {
+                        // No paramID. Interpret this as create
                         rowObj.editStatus = PARAM_VALUE_STATUS.changed;
                     }
                 }
-                /*
-                if (rowObj.paramID === null ) {
-                    if ( validateInput(rowObj.value) ) {
-                        var param = {
-                            scenarioID: $scope.paramScenario.scenarioID,
-                            lciaMethodID: $scope.lciaMethod.lciaMethodID,
-                            flowID: rowObj.flowID,
-                            value: +rowObj.value
-                        };
-                        ParamModelService.createParam(param);
-                    }
-                } else {
-                    if (rowObj.value && rowObj.value !== "") {
-                        if ( validateInput(rowObj.value)) {
-                            ParamModelService.updateParam(rowObj.paramID, +rowObj.value);
-                        }
+                else {
+                    // No param value. If it has an ID, interpret this as delete.
+                    if (rowObj.paramID) {
+                        rowObj.editStatus = PARAM_VALUE_STATUS.changed;
                     } else {
-                        ParamModelService.deleteParam(rowObj.paramID);
+                        rowObj.editStatus = PARAM_VALUE_STATUS.unchanged;
                     }
                 }
-                */
             }
 
-        }]);
+            function getParamChange(lf) {
+                var obj = { paramID: lf.paramID, value: lf.value }
+                return obj;
+            }
+
+            }]);
